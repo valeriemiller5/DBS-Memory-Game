db310288a816eef19a84e861c9033bc0d87d7882
Valerie <valerie.flores64@gmail.com>
2019-08-14 12:59:17 -0700

    updated image locations, updated Card component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import characters from './characters.json';
-// import background from './background.json';
 
 class App extends Component {
   state = {
@@ -17,35 +16,34 @@ class App extends Component {
   };
   // create cards with images on the front and the same pattern on all on the back
   // user can "flip" two cards
-    // if they match, they stay face up and score is increased +1
-    // if they do not match, they "flip" back over
+  // if they match, they stay face up and score is increased +1
+  // if they do not match, they "flip" back over
   // once all card matches have been found, +10 to final score
   // game resets
-    // cards are shuffled and all are face down to start a new round
+  // cards are shuffled and all are face down to start a new round
   // users can log in and continue adding to their score each time they play the game
   // Generate two of each image to match together
-  
+
   componentDidMount() {
     this.shuffleCharacter(characters);
+    // console.log(characters);
   }
 
   // Handles card 'shuffle' each time an image card is clicked
+  // Fisher-Yates Shuffling Algorithm
   shuffleCharacter = arr => {
-    // assistance on shuffleCharacter method Googled from https://www.jstips.co/en/javascript/shuffle-an-array/ using Fisher-Yates Shuffling Algorithm
-    let i,
-        j,
-        temp;
-    for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = arr[i];
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
     }
     return arr;
   }
-  
+
   handleClick = name => {
-    let firstChoice = this.characters.filter(character => character.name === name);
+    let firstChoice = this.state.characters.filter(character => character.name === name);
+    console.log(this.state.characters)
     console.log(firstChoice);
     // let secondChoice = this.state.characters.map(character => character.name);
     // let newScore = this.state.currentScore + 1;
@@ -58,7 +56,7 @@ class App extends Component {
     // } else {
 
     // }
-      
+
   }
 
 
@@ -66,17 +64,22 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-          <Wrapper>
-            {this.state.characters.map(character => (
-              <Card
-                key={character.id}
-                name={character.name}
-                click={this.state.clicked}
-                image={character.image}
-                handleClick={this.handleClick}
-              />
-            ))}
-          </Wrapper>
+        <Wrapper>
+          {this.state.characters.filter(character => {
+            if(character.id === 49) {
+              return false;
+            }
+            return true;
+          }).map(character => ( 
+            <Card
+              key={character.id}
+              name={character.name}
+              click={this.state.clicked}
+              image={character.image}
+              handleClick={this.handleClick}
+            /> 
+          ))}
+        </Wrapper>
         <Footer />
       </div>
     );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,11 +6,11 @@ const Card = props => (
     <label>
       <input type="checkbox" />
       <div className="card">
+        <div className="card_face front">
+          <img alt={props.name} onClick={() => props.handleClick(props.id)} src={require("../../images/background.jpeg")} />
+        </div>
         <div className="card_face back">
           <img alt={props.name} onClick={() => props.handleClick(props.id)} src={require("../../images/" + props.image)} />
-        </div>
-        <div className="card_face front">
-          <img alt={props.name} onClick={() => props.handleClick(props.id)} src={require("../../bgimage/background.jpeg")} />
         </div>
       </div>
     </label>
